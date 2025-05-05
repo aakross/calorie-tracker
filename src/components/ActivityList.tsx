a2f652fb7d85b451ed8bb2d72be8d0e75644d93d
royal9619@gmail.com <naknin-byvpyg-0pIcra>
2025-05-05 16:56:39 -0600

    05/05/25

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,11 +1,17 @@
 import { Activity } from "../types";
+import { categories } from "../data/categories";
+import { useMemo } from "react";
 
 type ActivityListProps = {
   activities: Activity[];
 };
 
 export default function ActivityList({ activities }: ActivityListProps) {
-  console.log(activities);
+  const categoryName = useMemo(
+    () => (category: Activity["category"]) =>
+      categories.map((cat) => (cat.id === category ? cat.name : "")),
+    [activities]
+  );
 
   return (
     <>
@@ -18,7 +24,7 @@ export default function ActivityList({ activities }: ActivityListProps) {
           className="px-5 py-10 bg-white mt-5 flex justify-between shadow-2xl"
         >
           <div className="space-y-2 relative">
-            <p>{activity.category}</p>
+            <p>{categoryName(+activity.category)}</p>
             <p className="text-2xl font-bold pt-5">{activity.name}</p>
             <p className="font-black text-4xl text-lime-500">
               {activity.calories} <span>Calorias</span>
