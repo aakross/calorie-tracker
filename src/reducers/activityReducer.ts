8ca0d90f896819f8ac83caed0ba4f85ed7372d30
royal9619@gmail.com <naknin-byvpyg-0pIcra>
2025-07-29 16:57:13 -0600

    29/07/25

--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -2,9 +2,10 @@ import { Activity } from "../types";
 
 export type ActivityActions =
   | { type: "save-activity"; payload: { newActivity: Activity } }
-  | { type: "set-activeId"; payload: { id: Activity["id"] } };
+  | { type: "set-activeId"; payload: { id: Activity["id"] } }
+  | { type: "delete-activity"; payload: { id: Activity["id"] } };
 
-type ActivityState = {
+export type ActivityState = {
   activities: Activity[];
   activeId: Activity["id"];
 };
@@ -20,10 +21,18 @@ export const activityReducer = (
 ) => {
   if (action.type === "save-activity") {
     // Este codigo maneja la logica para actualizar el state
-
+    let updatedActivities: Activity[] = [];
+    if (state.activeId) {
+      updatedActivities = state.activities.map((activity) =>
+        activity.id === state.activeId ? action.payload.newActivity : activity
+      );
+    } else {
+      updatedActivities = [...state.activities, action.payload.newActivity];
+    }
     return {
       ...state,
-      activities: [...state.activities, action.payload.newActivity],
+      activities: updatedActivities,
+      activeId: "",
     };
   }
   if (action.type === "set-activeId") {
@@ -32,5 +41,15 @@ export const activityReducer = (
       activeId: action.payload.id,
     };
   }
+
+  if (action.type === "delete-activity") {
+    return {
+      ...state,
+      activities: state.activities.filter(
+        (activity) => activity.id !== action.payload.id
+      ),
+    };
+  }
+
   return state;
 };
